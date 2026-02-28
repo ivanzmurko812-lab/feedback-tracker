ef2dd15bb6cf5ad93d68038fe63a3baa7d9952e4
ivanzmurko812-lab <ivanzmurko812@gmail.com>
2026-02-28 09:49:27 +0100

    server.js

--- server.js
+++ server.js
@@ -602,14 +602,15 @@ app.post(
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// если index.html рядом с server.js — это ок
 app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const port = process.env.PORT || 3000;
 
 (async () => {
   await initDatabase();
   app.listen(port, () => console.log("Listening on", port));
-})();
-  }
 })();
